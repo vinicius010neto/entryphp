e29f8220722db041ff6ef8da118f6559ccd3b67d
vinicius010neto <74117559+vinicius010neto@users.noreply.github.com>
2020-11-07 20:03:40 -0300

    create teste.php
    Criação de um primeiro arquivo de php, testes de comandos em geral.

---
+++ teste.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+$massa = "massaaa";
+var_dump($massa); echo "<br>";
+echo "testão da" . $massa . "<br>";
+echo "rs"; 
+$lenzin = strlen($massa); echo "<br>";
+echo $lenzin;
+$letras = " v b v b v b teste lata"; echo "<br>";
+$textcount = str_word_count($letras);
+echo $textcount;
+#ESTUDAR OPERADOR TERNÁRIO
+?>
+
+</body>
+</html>
